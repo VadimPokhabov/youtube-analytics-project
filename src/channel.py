d1f3118a20f942302db1d22723c0e61752a81235
Vadim <VadimPokhabov@mail.ru>
2024-01-28 19:25:33 +0300

    Перенос расчётов из _init__ в print_info

--- src/channel.py
+++ src/channel.py
@@ -9,23 +9,21 @@ def printj(dict_to_print: dict) -> None:
 
 
 class Channel:
-
     """Класс для ютуб-канала"""
 
     api_key = os.getenv('YT_API_KEY')
 
     def __init__(self, channel_id: str) -> None:
-
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
 
         self.channel_id = channel_id
+
+    def print_info(self):
+        """Выводит в консоль информацию о канале."""
+
         api_key: str = os.getenv('YT_API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         channel_id = 'UCwHL6WHUarjGfUM_586me8w'
-        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
-    def print_info(self):
-
-        """Выводит в консоль информацию о канале."""
-
-        printj(self.channel)
+        printj(channel)
