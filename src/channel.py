be753a60153ee0086e559fb63c23affa7e4fdd3e
Vadim <VadimPokhabov@mail.ru>
2024-02-01 02:32:04 +0300

    add magic

--- src/channel.py
+++ src/channel.py
@@ -4,11 +4,15 @@ from googleapiclient.discovery import build
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """
+    Класс для ютуб-канала
+    """
     api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """
+        Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
+        """
         self.__channel_id = channel_id
         self.channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = self.channel["items"][0]["snippet"]["title"]
@@ -19,11 +23,15 @@ class Channel:
         self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
+        """
+        Выводит в консоль информацию о канале.
+        """
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
     def to_json(self, file_name):
-        """Сохраняет в Json файл значения атрибутов экземпляра - информацию о канале"""
+        """
+        Сохраняет в Json файл значения атрибутов экземпляра - информацию о канале
+        """
         data = {'channel_id': self.channel_id,
                 'title': self.title,
                 'description': self.description,
@@ -37,9 +45,36 @@ class Channel:
 
     @classmethod
     def get_service(cls):
-        """Возвращает объект для работы с YouTube API"""
+        """
+        Возвращает объект для работы с YouTube API
+        """
         return build('youtube', 'v3', developerKey=Channel.api_key)
 
     @property
     def channel_id(self):
         return self.__channel_id
+
+    def __str__(self):
+        """
+        __str__
+        """
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        return self.view_count
+
+    def __sub__(self, other):
+        return self.view_count
+
+    def __le__(self, other):
+        return self.view_count
+
+    def __lt__(self, other):
+        return self.view_count
+
+    def __gt__(self, other):
+        return self.view_count
+
+    def __ge__(self, other):
+        return self.view_count
+
